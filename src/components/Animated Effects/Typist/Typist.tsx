4d0987a0cba1c787aa32c509589ce5ef3a9f02de
OlivierKDP <olivier.labbe@kdrp.com>
2023-05-04 16:21:34 -0400

    add-to-cart

--- src/components/Animated Effects/Typist/Typist.tsx
+++ src/components/Animated Effects/Typist/Typist.tsx
@@ -1,73 +1,74 @@
-import React, { useEffect, useRef } from 'react'
+import React, {useEffect, useRef} from 'react'
 import gsap from 'gsap'
-import { TypistStyled } from './typist.styled'
+import {TypistStyled} from './typist.styled'
 
 export interface TypingEffectProps {
-  message: string
-  speed?: speedType
-  showCursor?: boolean
+    message: string
+    className?: string
+    speed?: speedType
+    showCursor?: boolean
 }
 
 export type speedType = 'slow' | 'medium' | 'fast'
 
 const TypingCursor = () => {
-  return (
-    <div
-      style={{
-        backgroundColor: 'black',
-        width: 2,
-        height: 12,
-        marginLeft: 6,
-        display: 'inline-block'
-      }}
-    />
-  )
+    return (
+        <div
+            style={{
+                backgroundColor: 'black',
+                width: 2,
+                height: 12,
+                marginLeft: 6,
+                display: 'inline-block'
+            }}
+        />
+    )
 }
 
 const TypingEffect: React.FC<TypingEffectProps> = (
-  props: TypingEffectProps
+    props: TypingEffectProps
 ) => {
-  const textRefs = useRef<(HTMLSpanElement | null)[]>([])
-  textRefs.current = new Array(props.message.length).fill(null)
+    const textRefs = useRef<(HTMLSpanElement | null)[]>([])
+    textRefs.current = new Array(props.message.length).fill(null)
 
-  const speedValue = (value: speedType | undefined): number => {
-    switch (value) {
-      case 'slow':
-        return 0.1
-      case 'medium':
-        return 0.02
-      case 'fast':
-        return 0.01
-      default:
-        return 0.01
+    const speedValue = (value: speedType | undefined): number => {
+        switch (value) {
+            case 'slow':
+                return 0.1
+            case 'medium':
+                return 0.02
+            case 'fast':
+                return 0.01
+            default:
+                return 0.01
+        }
     }
-  }
 
-  useEffect(() => {
-    gsap.fromTo(
-      textRefs.current,
-      { autoAlpha: 0, y: 0, width: 0, display: 'none' },
-      {
-        width: 'auto',
-        autoAlpha: 12,
-        y: 0,
-        display: 'inline',
-        ease: 'none',
-        stagger: speedValue(props.speed) // Adjust this value to change the typing speed
-      }
-    )
-  }, [props.speed])
+    useEffect(() => {
+        gsap.fromTo(
+            textRefs.current,
+            {autoAlpha: 0, y: 0, width: 0, display: 'none'},
+            {
+                width: 'auto',
+                autoAlpha: 12,
+                y: 0,
+                display: 'inline',
+                ease: 'none',
+                stagger: speedValue(props.speed) // Adjust this value to change the typing speed
+            }
+        )
+    }, [props.speed])
 
-  return (
-    <TypistStyled>
-      {props.message.split('').map((char, index) => (
-        <span key={index} ref={(el) => (textRefs.current[index] = el)}>
+    return (
+        <TypistStyled className={props.className}>
+            {props.message.split('').map((char, index) => (
+                <span key={index} ref={(el) => (textRefs.current[index] = el)}>
           {char}
         </span>
-      ))}
-      {props.showCursor && <TypingCursor />}
-    </TypistStyled>
-  )
+            ))}
+            {props.showCursor && <TypingCursor/>}
+        </TypistStyled>
+    )
 }
 
 export default TypingEffect
