b45c5b8b642463dca3bb7ce377d60eb81bab6c43
OlivierKDP <olivier.labbe@kdrp.com>
2023-09-05 15:18:50 -0400

    perks messaging tooltip

--- src/components/Experimental/MemberPricingMessaging/MemberPricingMessaging.tsx
+++ src/components/Experimental/MemberPricingMessaging/MemberPricingMessaging.tsx
@@ -1,18 +1,39 @@
+import React, {useState} from 'react';
 import {Graphic, Igraphics} from "../../Graphic/Graphic";
-import { MemberPricingMessagingStyled } from "./MemberPricingMessagingStyled";
+import {MemberPricingMessagingStyled} from "./MemberPricingMessagingStyled";
 
 export interface iMemberPriceMessagingProps {
     priceLogo?: Igraphics;
-    infoFunction: ()=>void;
+    infoFunction: () => void;
 }
 
 export const MemberPriceMessaging = (props: iMemberPriceMessagingProps) => {
-    return(
+    const [tooltipVisible, setTooltipVisible] = useState(false);
+
+    return (
+
         <MemberPricingMessagingStyled className="member-price-display">
+            {tooltipVisible && (
+                <div className={"perks-tooltip"}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas eu
+                    urna eget felis dictum imperdiet. Mauris orci nisi, molestie at posuere eu, convallis eu ipsum.
+                </div>
+            )}
+
             <div className="label pricing-info">
-                <span>You save 25% with Auto-Delivery & <strong className="info-link" onClick={()=>props.infoFunction()}>Perks</strong></span>
+                <span>You save 25% with Auto-Delivery &
+                    <strong className="info-link"
+                            onClick={() => props.infoFunction()}
+                            onMouseEnter={() => setTooltipVisible(true)}
+                            onMouseLeave={() => setTooltipVisible(false)}>Perks
+                        {tooltipVisible && (
+                            <div className={"tooltip-caret"}>
+                            </div>
+                        )}
+                        <Graphic graphicName="perks-logo-small"/>
+                        </strong>
+                </span>
                 <span>
-                    <Graphic graphicName="perks-logo-small"/>
+
                 </span>
             </div>
         </MemberPricingMessagingStyled>
--- src/components/Experimental/MemberPricingMessaging/MemberPricingMessagingStyled.tsx
+++ src/components/Experimental/MemberPricingMessaging/MemberPricingMessagingStyled.tsx
@@ -4,7 +4,7 @@ import {colorNameToValue} from "../../_utilities/color-name-to-value/colorNameTo
 export const MemberPricingMessagingStyled = styled.div`
   --colorSpicedRed: ${colorNameToValue("perks-spiced-red")};
   --buttonLarge: 1.25rem;
-  --buttonMedium: 1 rem;
+  --buttonMedium: 1rem;
   --buttonSmall: 0.75rem;
 
   --FontSizeLarge: 1.25rem;
@@ -15,12 +15,15 @@ export const MemberPricingMessagingStyled = styled.div`
   --spacingMedium: 0.75rem;
   --spacingSmall: 0.5rem;
   --spacingTiny: 0.25rem;
-  
+
+
+  position: relative !important;
+
 
   .perks-logo-small {
     margin-left: 0.3rem;
-    width: 1.5rem;
-    height: 1.5rem;
+    width: 1rem;
+    height: 1rem;
     margin-top: -6px;
 
     .filled {
@@ -34,23 +37,88 @@ export const MemberPricingMessagingStyled = styled.div`
     align-content: center;
     flex-wrap: wrap;
     align-items: center;
-    
-    span{
+    width: 100%;
+
+    span {
       font-size: var(--FontSizeSmall);
     }
   }
-    .info-link {
-      text-decoration: underline;
-      color: var(--colorSpicedRed);
 
-      :hover {
-        background-color: var(--colorSpicedRed);
-        color: white;
-        padding: 0.2rem;
-        border-radius: 0.2rem;
+  .info-link {
+    text-decoration: underline;
+    color: var(--colorSpicedRed);
+    padding: 0.2rem;
+    position: relative;
+    display: inline-flex;
+    align-items: center;
+
+    :hover {
+      background-color: var(--colorSpicedRed);
+      color: white;
+      padding: 0.2rem;
+      border-radius: 0.2rem;
+      .filled{
+        fill:white;
       }
     }
+  }
 
+  /*  .perks-tooltip {
+      position: absolute;
+      background-color: #ffffff;
+      color: #3B2B2F;
+      !*color: var(--colordark-roast);*!
+      padding: 1rem;
+      border-radius: 5px;
+      marginTop: 2rem;
+      fontSize: 1rem;
+      z-index: 10000;
+      box-shadow: 0px 0px var(--spacingTiny) 0 rgb(0 0 0 / 30%);
+      border: 2px solid var(--colorSpicedRed);
+  
+    }
+      .perks-tooltip::before {
+        content: "";
+        position: absolute;
+        top: -10px; !* Adjust as necessary *!
+        left: 50%;
+        transform: translateX(-50%);
+        border-width: 10px;
+        border-style: solid;
+        border-color: transparent transparent var(--colorSpicedRed) transparent;
+      }*/
 
+  .perks-tooltip {
+    position: absolute;
+    background-color: #ffffff;
+    color: #3B2B2F;
+    padding: 1rem;
+    border-radius: 5px;
+    margin-top: 10px;
+    top: 100%;
+    font-size: 1rem;
+    z-index: 10000;
+    box-shadow: 0px 0px var(--spacingTiny) 0 rgb(0 0 0 / 30%);
+    left: 50%;
+    transform: translateX(-50%);
+    width: 90%;
+    /*
+    border: 2px solid var(--colorSpicedRed);
+    */
+  }
+
+  .tooltip-caret {
+    content: "";
+    position: absolute;
+    bottom: 100%;
+    left: 50%;
+    transform: translateX(-50%);
+    border-width: 12px;
+    border-style: solid;
+    border-color: transparent transparent white transparent;
+    z-index: 1000000;
+    top: 10px;
+  }
 
 `
+
